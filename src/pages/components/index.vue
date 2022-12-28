38d382f44730a984e2270eb27aef5039c316dcd7
tntgroup <dodu@live.cn>
2022-12-28 17:35:49 +0800

    feat: 加入组件模块占位

---
+++ src/pages/components/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <view class="_u_bg-white">
+    <u-skeleton rows="3" title loading></u-skeleton>
+  </view>
+</template>
+
+<style lang="scss" scoped></style>
