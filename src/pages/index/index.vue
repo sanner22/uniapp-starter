42b2bc6dfe4c852c0b6704063b5c0de06499a658
tntgroup <dodu@live.cn>
2023-01-05 15:09:23 +0800

    fix: 修复部分使用 u-button 页面的样式，与小程序保持一致

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,37 +1,16 @@
-<script setup lang="ts">
-  import AppProvider from '@/components/AppProvider/inedx.vue';
-  import { useRouter } from '@/hooks/router';
-  import { CURRENT_PLATFORM, PLATFORMS } from '@/enums/platformEnum';
-  import { judgePlatform } from '@/utils/platform';
-
-  const platform = CURRENT_PLATFORM;
-  const isVue3 = judgePlatform(PLATFORMS.VUE3);
-  const uViewVersion = uni.$u.config.v;
-
-  const router = useRouter();
-
-  const handleGetStarted = () => {
-    router.pushTab('/pages/components/index');
-    // router.push('/pages/log/index?id=4345&title=log');
-  };
-  const handleGoDemo = () => {
-    router.pushTab('/pages/demo/index');
-  };
-</script>
-
 <template>
   <!-- 自定义导航栏 -->
   <u-navbar title="首页" bg-color="#f6f6f6" placeholder>
     <template #left>
       <view class="mp-weixin-menu">
-        <u-icon name="arrow-left" size="18" color="black"></u-icon>
+        <u-icon name="arrow-left" size="18" color="black" />
         <u-line
           direction="column"
           :hairline="false"
           length="16"
           margin="0 8px"
-        ></u-line>
-        <u-icon name="home" size="20" color="black"></u-icon>
+        />
+        <u-icon name="home" size="20" color="black" />
       </view>
     </template>
   </u-navbar>
@@ -61,76 +40,99 @@
         :text="isVue3 ? 'Vue:3' : 'Vue:2'"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         bg-color="#bd34fe"
         border-color="#bd34fe"
         text="Vite:4"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         bg-color="#007acd"
         border-color="#007acd"
         text="TS:4.9.4"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         bg-color="#cca438"
         border-color="#cca438"
         text="pinia:2.0.28"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         type="primary"
         :text="`uView-plus:${uViewVersion}`"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         bg-color="#2b993a"
         border-color="#2b993a"
         :text="`UNIAPP`"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         bg-color="#30a15f"
         border-color="#30a15f"
         :text="`${platform}`"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
       <u-tag
         bg-color="#666666"
         border-color="#666666"
         text="UnoCSS:0.46.5"
         shape="circle"
         size="mini"
-      ></u-tag>
+      />
     </view>
 
     <view class="_u_center _u_gap-2 _u_mt-40px">
-      <u-button
-        type="primary"
-        size="small"
-        color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
-        @click="handleGetStarted"
-      >
-        快速开始 🚀
-      </u-button>
-      <u-button
-        type="primary"
-        size="small"
-        color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
-        @click="handleGoDemo"
-      >
-        demo ✨
-      </u-button>
+      <view>
+        <u-button
+          type="primary"
+          size="small"
+          color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
+          @click="handleGetStarted"
+          text="快速开始 🚀"
+        />
+      </view>
+      <view>
+        <u-button
+          type="primary"
+          size="small"
+          color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
+          @click="handleGoDemo"
+          text="demo ✨"
+        />
+      </view>
     </view>
   </app-provider>
 </template>
+
+<script setup lang="ts">
+  import AppProvider from '@/components/AppProvider/inedx.vue';
+  import { useRouter } from '@/hooks/router';
+  import { CURRENT_PLATFORM, PLATFORMS } from '@/enums/platformEnum';
+  import { judgePlatform } from '@/utils/platform';
+
+  const platform = CURRENT_PLATFORM;
+  const isVue3 = judgePlatform(PLATFORMS.VUE3);
+  const uViewVersion = uni.$u.config.v;
+
+  const router = useRouter();
+
+  const handleGetStarted = () => {
+    router.pushTab('/pages/components/index');
+    // router.push('/pages/log/index?id=4345&title=log');
+  };
+  const handleGoDemo = () => {
+    router.pushTab('/pages/demo/index');
+  };
+</script>
 
 <style lang="scss" scoped></style>
--- src/pagesA/base/button.vue
+++ src/pagesA/base/button.vue
@@ -1,99 +1,108 @@
-<script lang="ts" setup></script>
-
 <template>
   <view class="_u_px-15px">
-    <u-text text="按钮类型" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
-      <u-button>默认按钮</u-button>
-      <u-button type="primary">主要按钮</u-button>
-      <u-button type="success">成功按钮</u-button>
-      <u-button type="warning">警告按钮</u-button>
-      <u-button type="error">危险按钮</u-button>
+    <u-text text="按钮类型" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
+      <u-button text="默认按钮" />
+      <u-button type="primary" text="主要按钮" />
+      <u-button type="success" text="成功按钮" />
+      <u-button type="warning" text="警告按钮" />
+      <u-button type="error" text="危险按钮" />
     </view>
 
-    <u-text text="镂空按钮" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
-      <u-button plain :hairline="false">默认按钮</u-button>
-      <u-button type="primary" plain :hairline="false">主要按钮</u-button>
-      <u-button type="success" plain :hairline="false">成功按钮</u-button>
-      <u-button type="warning" plain :hairline="false">警告按钮</u-button>
-      <u-button type="error" plain :hairline="false">危险按钮</u-button>
+    <u-text text="镂空按钮" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
+      <u-button plain :hairline="false" text="默认按钮" />
+      <u-button type="primary" plain :hairline="false" text="主要按钮" />
+      <u-button type="success" plain :hairline="false" text="成功按钮" />
+      <u-button type="warning" plain :hairline="false" text="警告按钮" />
+      <u-button type="error" plain :hairline="false" text="危险按钮" />
     </view>
 
-    <u-text text="细边按钮" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
-      <u-button plain hairline>默认按钮</u-button>
-      <u-button type="primary" plain hairline>主要按钮</u-button>
-      <u-button type="success" plain hairline>成功按钮</u-button>
-      <u-button type="warning" plain hairline>警告按钮</u-button>
-      <u-button type="error" plain hairline>危险按钮</u-button>
+    <u-text text="细边按钮" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
+      <u-button plain hairline text="默认按钮" />
+      <u-button type="primary" plain hairline text="主要按钮" />
+      <u-button type="success" plain hairline text="成功按钮" />
+      <u-button type="warning" plain hairline text="警告按钮" />
+      <u-button type="error" plain hairline text="危险按钮" />
     </view>
 
-    <u-text text="禁用按钮" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
-      <u-button disabled>默认按钮</u-button>
-      <u-button type="primary" disabled>主要按钮</u-button>
-      <u-button type="success" disabled>成功按钮</u-button>
-      <u-button type="warning" disabled>警告按钮</u-button>
-      <u-button type="error" disabled>危险按钮</u-button>
+    <u-text text="禁用按钮" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
+      <u-button disabled text="默认按钮" />
+      <u-button type="primary" disabled text="主要按钮" />
+      <u-button type="success" disabled text="成功按钮" />
+      <u-button type="warning" disabled text="警告按钮" />
+      <u-button type="error" disabled text="危险按钮" />
     </view>
 
-    <u-text text="加载中" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
+    <u-text text="加载中" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
       <u-button
         type="primary"
         loading
         loading-text="加载中"
         loading-mode="spinner"
-        >主要按钮</u-button
-      >
+        text="主要按钮"
+      />
       <u-button
         type="success"
         loading
         loading-text="加载中"
         loading-mode="circle"
-        >成功按钮</u-button
-      >
-      <u-button type="warning" loading>警告按钮</u-button>
+        text="成功按钮"
+      />
+      <u-button type="warning" loading text="警告按钮" />
     </view>
 
-    <u-text text="按钮图标&按钮形状" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
+    <u-text text="按钮图标&按钮形状" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
       <u-button icon="lock-opened-fill" plain>默认按钮</u-button>
-      <u-button icon="lock-opened-fill" type="primary" plain shape="circle">
-        主要按钮
-      </u-button>
+      <u-button
+        icon="lock-opened-fill"
+        type="primary"
+        plain
+        shape="circle"
+        text="主要按钮"
+      />
     </view>
 
-    <u-text text="自定义颜色" type="info" margin="10px 0"></u-text>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
+    <u-text text="自定义颜色" type="info" margin="10px 0" />
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
       <u-button
         text="渐变色按钮"
         size="normal"
         color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
-      ></u-button>
+      />
       <u-button
         text="渐变色按钮"
         size="normal"
         color="linear-gradient(to right, rgb(220, 194, 11), rgb(4, 151, 99))"
-      ></u-button>
-      <u-button
-        text="青绿色按钮"
-        size="normal"
-        color="rgb(10, 185, 156)"
-      ></u-button>
+      />
+      <u-button text="青绿色按钮" size="normal" color="rgb(10, 185, 156)" />
     </view>
 
-    <u-text text="自定义大小" type="info" margin="10px 0"></u-text>
+    <u-text text="自定义大小" type="info" margin="10px 0" />
     <view class="_u_mb-15px">
-      <u-button text="超大尺寸" size="large" type="success"></u-button>
+      <u-button text="超大尺寸" size="large" type="success" />
     </view>
-    <view class="_u_flex _u_flex-wrap _u_gap-3">
-      <u-button text="普通尺寸" size="normal" type="error"></u-button>
-      <u-button text="小型尺寸" size="small" type="primary"></u-button>
-      <u-button text="超小尺寸" size="mini" type="warning"></u-button>
+    <view class="_u_flex _u_flex-wrap _u_gap-3 u-button-box">
+      <u-button text="普通尺寸" size="normal" type="error" />
+      <u-button text="小型尺寸" size="small" type="primary" />
+      <u-button text="超小尺寸" size="mini" type="warning" />
     </view>
   </view>
+
+  <u-gap height="30" />
 </template>
 
-<style lang="scss" scoped></style>
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+  .u-button-box {
+    :deep(.u-button) {
+      width: auto !important;
+      margin: 0 !important;
+    }
+  }
+</style>
