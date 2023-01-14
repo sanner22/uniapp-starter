36ed27d38623257d84086836502894316515d13f
tntgroup <dodu@live.cn>
2023-01-14 09:50:11 +0800

    fix: 修复 @dcloudio/types 3.2.7 对 Uni 类型改变导致 扩展失效的问题

---
+++ src/types/shims.uni.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="@dcloudio/types" />
+
+// 为 UniApp.Uni 扩展 $u 属性
+declare interface Uni {
+  $u: any;
+}
--- src/types/uviewPlus.d.ts
+++ src/types/uviewPlus.d.ts
@@ -1,9 +1,2 @@
 // 参考：https://blog.csdn.net/qq1014156094/article/details/107316969
 declare module 'uview-plus';
-
-// 为 UniApp.Uni 扩展 $u 属性
-declare namespace UniNamespace {
-  export interface UniInterface {
-    $u?: any;
-  }
-}
