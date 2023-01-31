a3585ae06f6baa3666243a09ffabd1cb31ec5430
tntgroup <dodu@live.cn>
2023-01-31 14:36:37 +0800

    feat: 添加 more 组件示例

--- src/pages/components/index.vue
+++ src/pages/components/index.vue
@@ -34,6 +34,7 @@
         <tm-cell :margin="[0, 0]" title="描述 descriptions" url="/pagesTMUI/showData/descriptions" />
         <tm-cell :margin="[0, 0]" title="图片 image" url="/pagesTMUI/showData/image" />
         <tm-cell :margin="[0, 0]" title="列表索引 indexes" url="/pagesTMUI/showData/indexes" />
+        <tm-cell :margin="[0, 0]" title="显示更多 more" url="/pagesTMUI/showData/more" />
       </view>
     </template>
 
