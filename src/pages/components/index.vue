17cd76acb5d8a8b7fb8b6f37afabd9906714a052
tnt group <dodu@live.cn>
2023-02-01 09:43:11 +0800

    feat: 添加 drawer 组件示例

--- src/pages/components/index.vue
+++ src/pages/components/index.vue
@@ -71,6 +71,7 @@
       <tm-text color="grey-darken-1" class="uno-m-15px">反馈：</tm-text>
       <view class="uno-mx-15px uno-rounded-3px uno-overflow-hidden">
         <tm-cell bottomBorder :margin="[0, 0]" title="警告提示 alert" url="/pages-tmui/feedback/alert" />
+        <tm-cell bottomBorder :margin="[0, 0]" title="抽屉 drawer" url="/pages-tmui/feedback/drawer" />
       </view>
     </template>
   </tm-app>
