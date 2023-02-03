4c229bc19ac8dbb4ffc3ffad2b92a8e8e107a74f
tntgroup <dodu@live.cn>
2023-02-04 02:27:02 +0800

    feat: 增加 unocss spacing 示例页面

---
+++ src/pages-unocss/spacing/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <view>
+    <view class="p-2">
+      <view class="uno-text-2xl uno-text-center uno-my-3"> padding </view>
+      <view class="uno-flex uno-flex-wrap">
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-0"> p-0 </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-1"> p-1 </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-2"> p-2 </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-3"> p-3 </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-4"> p-4 </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-none"> p-none </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p"> p </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-xs"> p-xs </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-sm"> p-sm </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-lg"> p-lg </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-xl"> p-xl </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center uno-p-2xl"> p-2xl </text>
+        <text class="uno-bg-green-100 uno-mr-2 uno-mb-2 uno-center -uno-p-2xl"> -p- </text>
+      </view>
+    </view>
+
+    <view class="p-2">
+      <view class="uno-text-2xl uno-text-center uno-my-3"> margin </view>
+      <view class="uno-flex uno-flex-wrap">
+        <text class="uno-bg-green-100 uno-center uno-m"> m </text>
+        <text class="uno-bg-green-100 uno-m-0"> m-0 </text>
+        <text class="uno-bg-green-100 uno-m-1"> m-1 </text>
+        <text class="uno-bg-green-100 uno-m-2"> m-2 </text>
+        <text class="uno-bg-green-100 uno-m-3"> m-3 </text>
+        <text class="uno-bg-green-100 uno-m-4"> m-4 </text>
+        <text class="uno-bg-green-100 uno-m-none"> m-none </text>
+        <text class="uno-bg-green-100 uno-m-xs"> m-xs </text>
+        <text class="uno-bg-green-100 uno-m-sm"> m-sm </text>
+        <text class="uno-bg-green-100 uno-m-md"> m-md </text>
+        <text class="uno-bg-green-100 uno-m-lg"> m-lg </text>
+        <text class="uno-bg-green-100 uno-m-xl"> m-xl </text>
+        <text class="uno-bg-green-100 uno-m-2xl"> m-2xl </text>
+      </view>
+    </view>
+
+    <view class="uno-p-2">
+      <text class="uno-bg-green-100 uno-top-20"> top </text>
+      <text class="uno-bg-green-100 uno-bottom-20"> bottom </text>
+      <text class="uno-bg-green-100 uno-left-20"> left </text>
+      <text class="uno-bg-green-100 uno-right-20"> right </text>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped></style>
