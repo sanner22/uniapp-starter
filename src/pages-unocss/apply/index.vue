400d592b260c76d6de77aefa0bcd1308dc6113a6
tntgroup <dodu@live.cn>
2023-02-04 03:52:11 +0800

    feat: 增加 unocss apply 示例页面(有bug,@apply 不生效)

---
+++ src/pages-unocss/apply/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="bg-to-t"> apply bg-to-t </view>
+
+  <view class="bg-unocss"> bg-unocss </view>
+
+  <view class="bg-to-r"> apply bg-to-r </view>
+</template>
+
+<style scoped>
+  .bg-to-t {
+    /* @apply uno-center uno-h-200 uno-rounded-md uno-bg-gradient-to-t uno-from-#f39c12/60 uno-via-#2ecc71:80 uno-to-#9b59b6_70 uno-mb-3; */
+  }
+
+  .bg-unocss{
+    /* @apply uno-w-100rpx uno-h-100rpx uno-bg-cover uno-mb-3 uno-bg-[url(https://img.cdn.sugarat.top/mdImg/MTY2ODA4OTc3MjcyMg==unocss-icon-gray.svg)] uno-m-auto; */
+  }
+
+  .bg-to-r{
+    --at-apply: uno-center uno-h-200rpx uno-rounded-md uno-bg-gradient-to-r uno-from-#f39c12/60 uno-via-#2ecc71:80 uno-to-#9b59b6_70 uno-mb-3
+  }
+</style>
