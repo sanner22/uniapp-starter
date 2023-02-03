0e84c210db9942229b61ca0f333aa4f7536b01a5
tntgroup <dodu@live.cn>
2023-02-04 03:13:57 +0800

    feat: 增加 unocss attributify-mode 示例页面

---
+++ src/pages-unocss/attributify-mode/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <view>
+    <button uno-text="sm green" uno-p="y-2 x-4" uno-m="4" uno-my-attr="y-1 x-2 sm"> Button </button>
+    <button uno-text-base uno-text-blue uno-py-2 uno-px-4 uno-m-4> 不会生效，规则必须时代值属性 </button>
+    <button uno-text="base blue" uno-py="2" uno-px="4" uno-m="4"> Button </button>
+    <button uno-border="~ red" uno-m="4"> Button </button>
+    <button uno-flex="~ col wrap" uno-class="m4"> Button </button>
+    <text uno-text="red"> This conflicts with links' text prop </text>
+    <view uno-bg="#333:30"> 111 </view>
+    <view uno-bg="[#11dddd]:80"> 222 </view>
+    <view uno-bg="#2ecc71:50"> 333 </view>
+  </view>
+</template>
+
+<style lang="scss" scoped></style>
