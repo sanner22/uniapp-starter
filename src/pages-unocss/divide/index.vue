558f39a669a96e7a6e5c78fef8b28e8e795548b7
tntgroup <dodu@live.cn>
2023-02-04 03:46:26 +0800

    feat: 增加 unocss divide 示例页面(有bug)

---
+++ src/pages-unocss/divide/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="uno-p-2 uno-pb-5 uno-space-y-5">
+    <!-- start: divide-x  -->
+    <view class="uno-text-xl uno-font-bold"> divide-x </view>
+
+    <view class="uno-flex uno-h-50rpx uno-divide-x uno-divide-yellow-500:50">
+      <view class="uno-flex-1 uno-center"> view </view>
+      <view class="uno-flex-1 uno-center"> view </view>
+      <view class="uno-flex-1 uno-center"> view </view>
+    </view>
+    <!-- end: divide-x  -->
+
+    <!-- start  -->
+    <view class="uno-text-xl uno-font-bold"> divide-y </view>
+
+    <view class="uno-divide-y uno-divide-blue">
+      <view class="uno-center"> view </view>
+      <view class="uno-center"> view </view>
+      <view class="uno-center"> view </view>
+    </view>
+    <!-- end  -->
+
+    <!-- start  -->
+    <view class="uno-text-xl uno-font-bold"> divide-(number) </view>
+
+    <view class="uno-divide-y-20 uno-divide-red-500 uno-divide-opacity-50">
+      <view class="uno-center"> view </view>
+      <view class="uno-center"> view </view>
+      <view class="uno-center"> view </view>
+    </view>
+    <!-- end  -->
+  </view>
+</template>
