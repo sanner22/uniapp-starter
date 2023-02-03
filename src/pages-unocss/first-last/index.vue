24becb7c8a88f90448edbdf8181a42a6b0b2342f
tntgroup <dodu@live.cn>
2023-02-04 03:30:01 +0800

    feat: 增加 unocss first-last 示例页面

---
+++ src/pages-unocss/first-last/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="uno-p-2">
+    <view>
+      <view class="uno-text-xl uno-font-bold"> first:class last:class </view>
+
+      <view class="uno-text-black:65 uno-text-sm">
+        <view> 第一个子元素为 bg-red </view>
+        <view> 最后一个子元素为 bg-green </view>
+        <view> 其他为 bg-blue </view>
+      </view>
+    </view>
+    <view>
+      <view class="first:uno-bg-red last:uno-bg-green uno-bg-blue uno-mb-1"> 1 </view>
+      <view class="first:uno-bg-red last:uno-bg-green uno-bg-blue uno-mb-1"> 2 </view>
+      <view class="first:uno-bg-red last:uno-bg-green uno-bg-blue uno-mb-1"> 3 </view>
+      <view class="first:uno-bg-red last:uno-bg-green uno-bg-blue uno-mb-1"> 4 </view>
+    </view>
+  </view>
+</template>
