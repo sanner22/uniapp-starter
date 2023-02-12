ea6e9d0e7dc89c575ea7fcafb69e3a320ef7f4e4
tntgroup <dodu@live.cn>
2023-02-12 11:27:12 +0800

    feat: 完善示例首页

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -8,46 +8,67 @@ const router = useRouter()
   <tm-app>
     <tm-navbar title="商城示例" :shadow="0" />
 
-    <tm-sheet :round="5" url="/pages-demo/mall/index">
-      <tm-text label="商城首页" :font-size="32" />
-      <tm-divider />
+    <tm-sheet :padding="[24, 0]" :round="5">
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="商城首页" label="商城示例模板" right-text="进入"
+        url="/pages-demo/mall/index" bottom-border
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-md-home" />
+        </template>
+      </tm-cell>
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="分类" label="商城示例模板" right-text="进入"
+        url="/pages-demo/mall/category" bottom-border
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-all-fill" />
+        </template>
+      </tm-cell>
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="商品列表" label="商城示例模板" right-text="进入"
+        url="/pages-demo/mall/list" bottom-border
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-alignright-fill" />
+        </template>
+      </tm-cell>
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="商品详情" label="商城示例模板" right-text="进入"
+        url="/pages-demo/mall/detail" bottom-border
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-ios-filing" />
+        </template>
+      </tm-cell>
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="购物车" label="商城示例模板" right-text="进入"
+        url="/pages-demo/mall/cart"
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-shopping-cart-fill" />
+        </template>
+      </tm-cell>
     </tm-sheet>
 
-    <tm-sheet :round="5" :margin="[32, 0, 32, 32]" url="/pages-demo/mall/category">
-      <tm-text label="分类" :font-size="32" />
-      <tm-divider />
+    <tm-sheet :margin="[32, 0, 32, 32]" :padding="[24, 0]" :round="5">
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="用户中心" label="商城示例模板" right-text="进入"
+        url="/pages/user/index" bottom-border @click="router.go('/pages/user/index')"
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-user-fill" />
+        </template>
+      </tm-cell>
+      <tm-cell
+        show-avatar :round="3" :margin="[0]" :padding="[0, 16]" title="用户登录" label="商城示例模板" right-text="进入"
+        url="/pages/login/index" bottom-border
+      >
+        <template #avatar>
+          <tm-icon color="orange" :font-size="38" name="tmicon-unlock-fill" />
+        </template>
+      </tm-cell>
     </tm-sheet>
-
-    <tm-sheet :round="5" :margin="[32, 0, 32, 32]" url="/pages-demo/mall/list">
-      <tm-text label="商品列表" :font-size="32" />
-      <tm-divider />
-    </tm-sheet>
-
-    <tm-sheet :round="5" :margin="[32, 0, 32, 32]" url="/pages-demo/mall/detail">
-      <tm-text label="商品详情" :font-size="32" />
-      <tm-divider />
-    </tm-sheet>
-
-    <tm-sheet :round="5" :margin="[32, 0, 32, 32]" url="/pages-demo/mall/cart">
-      <tm-text label="购物车" :font-size="32" />
-      <tm-divider />
-    </tm-sheet>
-
-    <tm-sheet :round="5" :margin="[32, 0, 32, 32]" url="/pages/user/index" @click="router.go('/pages/user/index')">
-      <tm-text label="用户中心" :font-size="32" />
-      <tm-divider />
-    </tm-sheet>
-
-    <tm-sheet :round="5" :margin="[32, 0, 32, 32]" url="/pages/login/index">
-      <tm-text label="用户登录" :font-size="32" />
-      <tm-divider />
-    </tm-sheet>
-
-    <!-- <view class="uno-p-15px">
-      <tm-button color="primary" size="small" block class="uno-flex uno-flex-row" _class="uno-flex-auto" @click="jumpList1">
-        List1 →
-      </tm-button>
-    </view> -->
   </tm-app>
 </template>
 
