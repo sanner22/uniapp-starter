f995f76bce2da6503931400489e2f1dbcc5d2f9b
tntgroup <dodu@live.cn>
2023-03-18 12:58:55 +0800

    fix: 更新 button 示例及

--- src/pages-tmui/common/button.vue
+++ src/pages-tmui/common/button.vue
@@ -6,15 +6,16 @@
       <tm-text :font-size="24" _class="font-weight-b" label="基本示例" />
       <tm-divider />
       <view class="flex flex-row flex-wrap">
-        <tm-button :margin="[10]" size="mini" :shadow="0" label="mini" />
+        <tm-button :width="120" :margin="[10]" :shadow="0" size="large" label="large" />
+        <tm-button :margin="[10]" :shadow="0" size="normal" label="normal" />
         <tm-button :margin="[10]" size="small" :shadow="0" label="small" />
-        <tm-button :margin="[10]" :shadow="0" text size="normal" label="normal" />
-        <tm-button :margin="[10]" :shadow="0" text :border="2" border-style="dashed" size="large" label="large" />
+        <tm-button :margin="[10]" size="mini" :shadow="0" label="mini" />
+
         <tm-button :margin="[10]" :shadow="0" text size="normal" outlined label="浅边框" />
         <tm-button :margin="[10]" :shadow="0" :border="6" outlined size="normal" label="深边框" />
         <tm-button :margin="[10]" transprent text :shadow="0" size="normal" label="透明背景" />
       </view>
-      <tm-button block label="block" />
+      <tm-button block label="block" class="uno-flex uno-flex-row" _class="uno-flex-auto" />
     </tm-sheet>
     <tm-sheet>
       <tm-text :font-size="24" _class="font-weight-b" label="带图标" />
@@ -58,16 +59,21 @@
       </view>
     </tm-sheet>
     <tm-sheet>
+      <tm-text :font-size="24" _class="font-weight-b" label="带图标" />
+      <tm-divider />
+      <view class="flex flex-row flex-wrap">
+        <tm-button :margin="[0]" disabled icon="tmicon-tongzhifill" size="normal" label="禁用" />
+        <tm-button :margin="[24, 0]" loading icon="tmicon-tongzhifill" size="normal" label="加载中" />
+        <tm-button :margin="[0, 24]" icon="tmicon-tongzhifill" size="normal" label="正常" />
+      </view>
+    </tm-sheet>
+    <tm-sheet>
       <tm-text :font-size="24" _class="font-weight-b" label="渐变样式,更多属性见文档" />
       <tm-divider />
-      <tm-button loading linear="right" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="light 加载中" />
-      <tm-button linear="right" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="light" />
-      <tm-button linear="right" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="light 被禁用" disabled />
-      <tm-button linear="right" linear-deep="dark" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="dark" />
-      <tm-button linear="right" linear-deep="dark" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="dark 被禁用" disabled />
-      <tm-button linear="right" linear-deep="accent" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="accent" />
-      <tm-button color="pink" linear="right" linear-deep="accent" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="accent" />
-      <tm-button color="orange" linear="right" linear-deep="accent" block class="uno-flex uno-flex-row" _class="uno-flex-auto" label="accent top" />
+      <tm-button class="uno-flex uno-flex-row" _class="uno-flex-auto" :linear-color="['#ea3c2d', '#ff9d14']" color="orange" font-color="white" linear="left" block label="自定义渐变背景" />
+      <tm-button class="uno-flex uno-flex-row" _class="uno-flex-auto" linear="right" block label="light" />
+      <tm-button class="uno-flex uno-flex-row" _class="uno-flex-auto" linear="right" linear-deep="dark" block label="dark" />
+      <tm-button class="uno-flex uno-flex-row" _class="uno-flex-auto" linear="right" linear-deep="accent" block label="accent" />
     </tm-sheet>
   </tm-app>
 </template>
